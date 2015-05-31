8621e0478d0eeb5996518c0af8cc757d67113a12
andryw <andryw.neo@gmail.com>
2015-05-31 17:25:32 -0300

    merge

--- analysis/simple-todos.js
+++ analysis/simple-todos.js
@@ -3,7 +3,7 @@
 if (Meteor.isClient) {
 
     Meteor.subscribe('bandas2');
-    Programacao = new Mongo.Collection('bandas2');
+    Programacao = new Mongo.Collection('media_dias');
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
@@ -15,21 +15,15 @@ if (Meteor.isClient) {
 
         "click .toggle-checked": function () {
             // Set the checked property to the opposite of its current value
-            var bandas = this.bandas;
-            var valores = {}
-            var total = 0;
-            for(var i = 0; i < bandas.length; i++){
-                var banda = bandas[0];
-                var tags = banda.tags;
+            var datas = this.datas;
+            var csv = []
+            for(var i = 0; i < datas.length; i++){
+                var data = datas[0];
+                var tags = data.media_tags;
                 for(var j = 0; j < tags.length; j++){
                     var tagName = tags[j].tag
                     var tagValue = tags[j].value
-
-                    if (! (tagName in valores)){
-                        valores[tagName] = 0
-                    }
-                    valores[tagName] +=tagValue
-                    total += tagValue
+                    csv.push({"date":data,"price":tagValue,"symbol":tagName})
                 }
             }
 
@@ -45,7 +39,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
     Meteor.startup(function () {
         var d = new MongoInternals.RemoteCollectionDriver("mongodb://andryw:4n4lytics@ds033607.mongolab.com:33607/raiox-sj");
-        Tags = new Mongo.Collection('bandas2', { _driver: d });
+        Tags = new Mongo.Collection('media_dias', { _driver: d });
 
         Meteor.publish('bandas2', function(){
             return Tags.find();
