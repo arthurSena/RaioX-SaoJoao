3352d5d97a80bae950fcaf2dcba78e9567166ab0
Tercio de Melo <tercio.melo@topfreegames.com>
2015-05-31 17:24:48 -0300

    Prittification [2]

--- analysis/simple-todos.js
+++ analysis/simple-todos.js
@@ -2,8 +2,8 @@
 
 if (Meteor.isClient) {
 
-    Meteor.subscribe('bandas2');
-    Programacao = new Mongo.Collection('bandas2');
+  Meteor.subscribe('media_dias');
+  Programacao = new Mongo.Collection('media_dias');
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
@@ -11,46 +11,46 @@ if (Meteor.isClient) {
     }
   });
 
-    Template.task.events({
+  Template.task.events({
 
-        "click .toggle-checked": function () {
-            // Set the checked property to the opposite of its current value
-            var bandas = this.bandas;
-            var valores = {}
-            var total = 0;
-            for(var i = 0; i < bandas.length; i++){
-                var banda = bandas[0];
-                var tags = banda.tags;
-                for(var j = 0; j < tags.length; j++){
-                    var tagName = tags[j].tag
-                    var tagValue = tags[j].value
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      var bandas = this.bandas;
+      var valores = {}
+      var total = 0;
+      for(var i = 0; i < bandas.length; i++){
+        var banda = bandas[0];
+        var tags = banda.tags;
+        for(var j = 0; j < tags.length; j++){
+          var tagName = tags[j].tag
+            var tagValue = tags[j].value
 
-                    if (! (tagName in valores)){
-                        valores[tagName] = 0
-                    }
-                    valores[tagName] +=tagValue
-                    total += tagValue
-                }
+            if (! (tagName in valores)){
+              valores[tagName] = 0
             }
-
-           console.log("oi");
+          valores[tagName] +=tagValue
+            total += tagValue
         }
+      }
+
+      console.log("oi");
+    }
 
 
-    });
+  });
 
 }
 
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-        var d = new MongoInternals.RemoteCollectionDriver("mongodb://andryw:4n4lytics@ds033607.mongolab.com:33607/raiox-sj");
-        Tags = new Mongo.Collection('bandas2', { _driver: d });
+  Meteor.startup(function () {
+    var d = new MongoInternals.RemoteCollectionDriver("mongodb://andryw:4n4lytics@ds033607.mongolab.com:33607/raiox-sj");
+    Tags = new Mongo.Collection('media_dias', { _driver: d });
 
-        Meteor.publish('bandas2', function(){
-            return Tags.find();
-        });
-
+    Meteor.publish('media_dias', function(){
+      return Tags.find();
     });
+
+  });
 
 }
